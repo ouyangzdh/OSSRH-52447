2a780f9d3f81cbab0adf074460d66ad235bb0447
zhangdonghai <18953219501@sian.cn>
2019-10-27 22:37:02 +0800

    1

---
+++ shanhi-server/src/main/java/com/shanhi/util/DateUtil.java
@@ -0,0 +1,13 @@
+package com.shanhi.util;
+
+/**
+ * @author donghai.zhang
+ * @date 2019
+ * @describe
+ */
+public class DateUtil {
+
+
+
+
+}
---
+++ shanhi-server/src/test/java/com/shanhi/ShanhiServerApplicationTests.java
@@ -0,0 +1,16 @@
+//package com.shanhi.shanhiserver;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class ShanhiServerApplicationTests {
+//
+//    @Test
+//    public void contextLoads() {
+//    }
+//
+//}
